88a7a8234dc86e24193478478f81e9e9b24fe84b
Tommy Li <Tommy.chaoping.li@ibm.com>
2019-04-17 11:27:40 -0700

    update ibm train components;

--- components/ibm-components/ffdl/train/src/train.py
+++ components/ibm-components/ffdl/train/src/train.py
@@ -20,6 +20,12 @@ from ruamel.yaml import YAML
 import subprocess
 import os
 
+def get_secret(path):
+    with open(path, 'r') as f:
+        cred = f.readline().strip('\'')
+    f.close()
+    return cred
+
 ''' global initialization '''
 yaml = YAML(typ='safe')
 
@@ -27,30 +33,74 @@ yaml = YAML(typ='safe')
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_def_file_path', type=str, help='Object storage bucket file path for the training model definition')
-    parser.add_argument('--manifest_file_path', type=str, help='Object storage bucket file path for the FfDL manifest')
+    parser.add_argument('--manifest_file_path', type=str, help='Object storage bucket file path for the FfDL manifest', default="")
+    parser.add_argument('--training_job_name', type=str, help='Name of the training job', default="training job")
+    parser.add_argument('--training_job_description', type=str, help='description of the training job', default="Training ML/DL model")
+    parser.add_argument('--training_job_version', type=str, help='version of the training job', default="1.0")
+    parser.add_argument('--training_job_gpus', type=float, help='number of gpus for the training job', default=0)
+    parser.add_argument('--training_job_cpus', type=float, help='number of cpus for the training job', default=1.0)
+    parser.add_argument('--training_job_learners', type=int, help='number of learners for the training job', default=1)
+    parser.add_argument('--training_job_memory', type=str, help='Amount of memory for the training job', default="1Gb")
+    parser.add_argument('--training_job_framework_name', type=str, help='ML/DL Framework name for the training job', default="tensorflow")
+    parser.add_argument('--training_job_framework_version', type=str, help='ML/DL Framework version for the training job', default="latest")
+    parser.add_argument('--training_job_command', type=str, help='Exuecution command for the training job', default="echo command not defined")
+    parser.add_argument('--training_job_metric_type', type=str, help='Metric storing type for the training job', default="tensorboard")
+    parser.add_argument('--training_job_metric_path', type=str, help='Metric storing path for the training job', default="$JOB_STATE_DIR/logs/tb")
     args = parser.parse_args()
 
-    with open("/app/secrets/s3_url", 'r') as f:
-        s3_url = f.readline().strip('\'')
-    f.close()
-    with open("/app/secrets/training_bucket", 'r') as f:
-        data_bucket_name = f.readline().strip('\'')
-    f.close()
-    with open("/app/secrets/result_bucket", 'r') as f:
-        result_bucket_name = f.readline().strip('\'')
-    f.close()
-    with open("/app/secrets/s3_access_key_id", 'r') as f:
-        s3_access_key_id = f.readline().strip('\'')
-    f.close()
-    with open("/app/secrets/s3_secret_access_key", 'r') as f:
-        s3_secret_access_key = f.readline().strip('\'')
-    f.close()
-    with open("/app/secrets/ffdl_rest", 'r') as f:
-        ffdl_rest = f.readline().strip('\'')
-    f.close()
+    s3_url = get_secret("/app/secrets/s3_url")
+    data_bucket_name = get_secret("/app/secrets/training_bucket")
+    result_bucket_name = get_secret("/app/secrets/result_bucket")
+    s3_access_key_id = get_secret("/app/secrets/s3_access_key_id")
+    s3_secret_access_key = get_secret("/app/secrets/s3_secret_access_key")
+    ffdl_rest = get_secret("/app/secrets/ffdl_rest")
 
     model_def_file_path = args.model_def_file_path
     manifest_file_path = args.manifest_file_path
+    training_job_name = args.training_job_name
+    training_job_description = args.training_job_description
+    training_job_version = args.training_job_version
+    training_job_gpus = args.training_job_gpus
+    training_job_cpus = args.training_job_cpus
+    training_job_learners = args.training_job_learners
+    training_job_memory = args.training_job_memory
+    training_job_framework_name = args.training_job_framework_name
+    training_job_framework_version = args.training_job_framework_version
+    training_job_command = args.training_job_command
+    training_job_metric_type = args.training_job_metric_type
+    training_job_metric_path = args.training_job_metric_path
+
+    ffdl_template = {
+        "name": training_job_name,
+        "description": training_job_description,
+        "version": training_job_version,
+        "gpus": training_job_gpus,
+        "cpus": training_job_cpus,
+        "learners": training_job_learners,
+        "memory": training_job_memory,
+        "data_stores": [
+            {
+                "id": "cos",
+                "type": "mount_cos",
+                "training_data": {"container": data_bucket_name},
+                "training_results": {"container": result_bucket_name},
+                "connection": {
+                    "auth_url": s3_url,
+                    "user_name": s3_access_key_id,
+                    "password": s3_secret_access_key
+                }
+            }
+        ],
+        "framework": {
+            "name": training_job_framework_name,
+            "version": training_job_framework_version,
+            "command": training_job_command
+        },
+        "evaluation_metrics": {
+            "type": training_job_metric_type,
+            "in": training_job_metric_path
+        }
+    }
 
     ''' Download FfDL CLI for log streaming '''
     res = requests.get('https://github.com/IBM/FfDL/raw/master/cli/bin/ffdl-linux', allow_redirects=True)
@@ -68,7 +118,8 @@ if __name__ == "__main__":
 
     try:
         client.Bucket(data_bucket_name).download_file(model_def_file_path, 'model.zip')
-        client.Bucket(data_bucket_name).download_file(manifest_file_path, 'manifest.yml')
+        if manifest_file_path:
+            client.Bucket(data_bucket_name).download_file(manifest_file_path, 'manifest.yml')
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
@@ -76,10 +127,12 @@ if __name__ == "__main__":
             raise
 
     ''' Update FfDL manifest with the corresponding object storage credentials '''
-
-    f = open('manifest.yml', 'r')
-    manifest = yaml.load(f.read())
-    f.close()
+    if manifest_file_path:
+        f = open('manifest.yml', 'r')
+        manifest = yaml.load(f.read())
+        f.close()
+    else:
+        manifest = ffdl_template
 
     manifest['data_stores'][0]['connection']['auth_url'] = s3_url
     manifest['data_stores'][0]['connection']['user_name'] = s3_access_key_id
